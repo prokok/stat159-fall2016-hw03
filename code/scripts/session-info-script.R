bbbd80d4df0532a0977a730cf2c64ec868dcf03e
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-10 23:57:41 -0700

    changes in space lines in codes for readability

--- code/scripts/session-info-script.R
+++ code/scripts/session-info-script.R
@@ -1,6 +1,8 @@
 .libPaths(c("C:/Users/vlfgn/Documents/R/win-library/3.3", "C:/Users/vlfgn/Documents/R/win-library/3.3"))
 library(xtable)
 library(rmarkdown)
+library(testthat)
+
 
 sink(file = "../../session-Info.txt")
 cat("-Session Info-\n\n")
